193b909dfd614c9613b95d9f547a1b4a4471413b
Revanth Ayodhya <revanthayodhya@gmail.com>
2025-05-22 21:58:25 +0530

    created password generator

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+const password1 = document.getElementById("value1");
+const password2 = document.getElementById("value2");
+const generateBtn = document.getElementById("generate");
+
+function getRandomCharacter() {
+  const index = Math.floor(Math.random() * characters.length);
+  return characters[index];
+}
+
+function generatePasswords(length = 15) {
+  let pwd1 = "",
+    pwd2 = "";
+  for (let i = 0; i < length; i++) {
+    pwd1 += getRandomCharacter();
+    pwd2 += getRandomCharacter();
+  }
+  password1.textContent = pwd1;
+  password2.textContent = pwd2;
+}
+
+generateBtn.addEventListener("click", () => {
+  generatePasswords();
+});
